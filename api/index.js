08dac7985329c0fbadc5829fbb29996e34db2330
dell <yshondre@gmail.com>
2024-11-14 22:22:39 +0100

    connecte to mongodv

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
-  console.log("Server is running on port 30000");
+  console.log("Server is running on port 3000");
 });
