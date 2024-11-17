3e99b5279a7ad6a960dfa7faede5e39e86058e6e
dell <yshondre@gmail.com>
2024-11-17 14:48:43 +0100

    sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { handleError } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
@@ -22,3 +23,43 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (email === "" || password === "") {
+    return next(handleError(400, "All fields must be provided"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(handleError(404, "Wrong credentials!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(handleError(401, "Wrong credentials!"));
+    const { password: pass, ...userInfo } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(userInfo);
+  } catch (error) {
+    next(error);
+  }
+};
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   try {
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) return next(errorHandler(404, "Wrong credentials!"));
+//     const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+//     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+//     const { password: pass, ...userInfo } = validUser._doc; // ici je recup les toutes les info de l'utilisateur a l'aide du ...userInfo sauf le password que je stock dans pass
+//     res
+//       .cookie("access_token", token, { httpOnly: true })
+//       .status(200)
+//       .json(userInfo);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,45 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [formdata, setFormeData] = useState([]);
-  const handleSubmit = () => {};
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  console.log(loading);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -23,10 +58,15 @@ export default function SignUp() {
         </div>
         {/* right*/}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput id="username" type="text" placeholder="Username" />
+              <TextInput
+                id="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your email" />
@@ -34,17 +74,29 @@ export default function SignUp() {
                 id="email"
                 type="text"
                 placeholder="name@company.com"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput id="password" type="password" />
+              <TextInput
+                id="password"
+                type="password"
+                onChange={handleChange}
+              />
             </div>
             <Button
               type="submit"
               className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
             >
-              Sign up
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign in"
+              )}
             </Button>
           </form>
           <div className="flex gap-3 items-center text-sm mt-5">
@@ -53,6 +105,11 @@ export default function SignUp() {
               Sign in
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
